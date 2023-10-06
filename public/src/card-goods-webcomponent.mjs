1e87ab1c1856d81bbdb68775c6f71b8ab2a2d661
alu0101056944 <alu0101056944@ull.edu.es>
2023-10-06 23:23:09 +0000

    Added variables to CardGoods for the length attribute to control the amount of placeholder cards the CardGoods webcomponent has.

--- public/src/card-goods-webcomponent.mjs
+++ public/src/card-goods-webcomponent.mjs
@@ -6,14 +6,26 @@
  */
 
 export default class CardGoods extends HTMLElement {
-  // static observedAttributes = [];
+  static observedAttributes = ['length'];
+  #amountOfPlaceholderCards = undefined;
+  #originalSlot = undefined;
+  #shadow = undefined;
 
   constructor() {
     super();
-    const shadow = this.attachShadow({mode: 'closed'});
+    this.#amountOfPlaceholderCards = this.length ? parseInt(this.length, 10) : 1;
     const template = document.getElementById('card-goods').content;
-    const clone = template.cloneNode(true);
-    shadow.append(clone);
+    const templateClone = template.cloneNode(true);
+    const originalSlot = templateClone.getElementById('card-goods-slot');
+    this.#originalSlot = originalSlot.clone(true);
+    originalSlot.remove();
+    for (let i = 1; i <= this.#amountOfPlaceholderCards; ++i) {
+      const newSlot = this.#originalSlot.cloneNode(true);
+      newSlot.id = newSlot.id + i;
+      templateClone.append(newSlot);
+    }
+    this.#shadow = this.attachShadow({mode: 'closed'});
+    this.#shadow.append(templateClone);
   }
 
   createdCallback() {
@@ -28,7 +40,22 @@ export default class CardGoods extends HTMLElement {
     console.log('WebComponent CardGoods disconnected.');
   }
 
-  attributeChangedCallback() {
-
+  attributeChangedCallback(attributeName, oldValue, newValue) {
+    if (attributeName === 'length') {
+      const newLength = parseInt(newValue, 10);
+      const oldLength = parseInt(oldValue, 10);
+      if (oldLength < newLength) {
+        for (let i = this.#amountOfPlaceholderCards; i >= newLength; i--) {
+          const slotToRemove = document.getElementById('card-goods-slot' + i);
+          slotToRemove.remove();
+        }
+      } else {
+        for (let i = oldLength + 1; i <= newLength; i++) {
+          const newSlot = this.#originalSlot.clone(true);
+          newSlot.id = newSlot.id + i;
+          this.#shadow.append(newSlot);
+        }
+      }
+    }
   }
 }
