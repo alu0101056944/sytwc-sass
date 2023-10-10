5f00216014863c1a006fc1f99fb1932ed08c904d
alu0101056944 <alu0101056944@ull.edu.es>
2023-10-10 21:49:44 +0000

    Attempt at adding a width and height attribute to card goods webcomp. WIP because it is not changing.

--- public/src/card-goods-webcomponent.mjs
+++ public/src/card-goods-webcomponent.mjs
@@ -8,14 +8,18 @@
 'use strict';
 
 export default class CardGoods extends HTMLElement {
-  static observedAttributes = ['length'];
+  static observedAttributes = ['length', 'width', 'height'];
   #amountOfPlaceholderCards = undefined;
   #originalSlot = undefined;
   #shadow = undefined;
   #mainDiv = undefined;
+  #cardWidth = undefined;
+  #cardHeight = undefined;
 
   constructor() {
     super();
+    this.#cardWidth = this.width ? parseInt(this.width) : 90;
+    this.#cardHeight = this.height ? parseInt(this.height) : 130;
     this.#amountOfPlaceholderCards = this.length ? parseInt(this.length, 10) : 1;
     const templateContent = document.getElementById('card-goods').content;
     const templateCloneContent = templateContent.cloneNode(true);
@@ -50,8 +54,8 @@ export default class CardGoods extends HTMLElement {
       }
       .examples .example {
         background-color: IndianRed;
-        width: 90px;
-        height: 130px;
+        width: ${this.#cardWidth}px;
+        height: ${this.#cardHeight}px;
         margin: 8px 8px 8px 8px;
         padding: 5px 5px 5px 5px;
         border: 1px solid dimgray;
@@ -92,6 +96,16 @@ export default class CardGoods extends HTMLElement {
           slotToRemove.remove();
         }
       }
+    } else if (attributeName === 'width') {
+      const exampleArray = this.#shadow.querySelectorAll('.example');
+      exampleArray.forEach((example) => {
+        example.style.width = newValue;
+      });
+    } else if (attributeName === 'height') {
+      const exampleArray = this.#shadow.querySelectorAll('.example');
+      exampleArray.forEach((example) => {
+        example.style.height = newValue;
+      });
     }
   }
 }
