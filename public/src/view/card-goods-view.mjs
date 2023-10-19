aac637db6721f01fe655e5fc6561b1fe8678f2d0
alu0101056944 <alu0101056944@ull.edu.es>
2023-10-19 21:54:09 +0000

    CardGoodsView got removed the css styling. _base.scss now defines examples style globally.

--- public/src/view/card-goods-view.mjs
+++ public/src/view/card-goods-view.mjs
@@ -28,30 +28,6 @@ export default class CardGoodsView {
                       .cloneNode(true);
     this.#shadow = this.attachShadow({mode: 'closed'});
     this.#shadow.append(content);
-    this.#setTemplateStyle();
-  }
-
-  #setTemplateStyle() {
-    const CSS_STYLING_STRING = `
-      .examples {
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: space-evenly;
-        padding: 10px 3px 2px 3px;
-        margin: 10px 3px 5px 3px;
-      }
-      .examples .example {
-        background-color: IndianRed;
-        width: ${this.#cardWidth}px;
-        height: ${this.#cardHeight}px;
-        margin: 8px 8px 8px 8px;
-        padding: 5px 5px 5px 5px;
-        border: 1px solid dimgray;
-        border-radius: 20px;
-      }`;
-    const styleElement = document.createElement('style');
-    styleElement.textContent = CSS_STYLING_STRING;
-    this.#shadow.append(styleElement);
   }
 
   updateCardWidth(newWidth) {
