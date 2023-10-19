e6269051c4ade78429dacc2fd744e7eb485e3f33
alu0101056944 <alu0101056944@ull.edu.es>
2023-10-19 21:50:46 +0000

    CardGoodsView templateDefinition and logic for slot duplication removed. Amount of cards logic removed since it is not done in DOM in index.html.

--- public/src/view/card-goods-view.mjs
+++ public/src/view/card-goods-view.mjs
@@ -15,57 +15,20 @@
  */
 export default class CardGoodsView {
   /** @private @constant */
-  #originalSlot = undefined;
   #shadow = undefined;
-  #mainDiv = undefined;
 
   /** @private */
-  #amountOfPlaceholderCards = undefined;
   #cardHeight = undefined;
   #cardWidth = undefined;
 
   constructor() {
-    const template = this.#defineTemplate();
-    const templateContent = template.content;
-    const templateContentClone = templateContent.cloneNode(true);
-    this.#originalSlot = this.#getSlotIn(templateContentClone);
-    this.#originalSlot.remove();
-    this.#mainDiv = templateContentClone.querySelector('.examples');
-    this.#appendSlotsIntoMainDiv();
+    const content = document
+                      .getElementById('card-goods-template')
+                      .content
+                      .cloneNode(true);
     this.#shadow = this.attachShadow({mode: 'closed'});
-    this.#shadow.append(templateContentClone);
+    this.#shadow.append(content);
     this.#setTemplateStyle();
-  }
-
-  #defineTemplate() {
-    const template = document.createElement('template');
-    const content = template.content;
-    const divOutermost = document.createElement('div');
-    divOutermost.classList.add('examples');
-    content.append(divOutermost);
-    const slotForFutureDeletion = document.createElement('slot');
-    divOutermost.append(slotForFutureDeletion);
-    const divWithExample = document.createElement('div');
-    divWithExample.classList.add('example');
-    slotForFutureDeletion.append(divWithExample);
-    return template;
-  }
-
-  #getSlotIn(templateContent) {
-    const childSlots = templateContent.querySelectorAll('slot');
-    const originalSlot = childSlots[0];
-    if (!originalSlot) {
-      throw new Error('Template does not have any slots.');
-    }
-    return originalSlot;
-  }
-
-  #appendSlotsIntoMainDiv() {
-    for (let i = 1; i <= this.#amountOfPlaceholderCards; ++i) {
-      const newSlot = this.#originalSlot.cloneNode(true);
-      newSlot.name = 'card-goods-slot' + i;
-      this.#mainDiv.append(newSlot);
-    }
   }
 
   #setTemplateStyle() {
@@ -89,30 +52,6 @@ export default class CardGoodsView {
     const styleElement = document.createElement('style');
     styleElement.textContent = CSS_STYLING_STRING;
     this.#shadow.append(styleElement);
-  }
-
-  updateCardAmount(oldAmount, newAmount) {
-    const newAmount = parseInt(newValue, 10);
-    let oldAmount;
-    if (oldValue) {
-      oldAmount = parseInt(oldValue, 10);
-    } else {
-      const initialValue = this.#amountOfPlaceholderCards;
-      oldAmount = initialValue;
-    }
-    if (oldAmount < newAmount) {
-      for (let i = oldAmount + 1; i <= newAmount; i++) {
-        const newSlot = this.#originalSlot.cloneNode(true);
-        newSlot.name = 'card-goods-slot' + i;
-        this.#mainDiv.append(newSlot);
-      }
-    } else {
-      for (let i = this.#amountOfPlaceholderCards; i >= newAmount; i--) {
-        const slotToRemove = document.getElementById('card-goods-slot' + i);
-        slotToRemove.remove();
-      }
-    }
-    this.#amountOfPlaceholderCards = newAmount;
   }
 
   updateCardWidth(newWidth) {
