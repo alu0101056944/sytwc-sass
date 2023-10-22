fea0b366f028cbef5e53539a76fcf4f981e227c3
alu0101056944 <alu0101056944@ull.edu.es>
2023-10-22 22:11:19 +0000

    Card view now has a span storage with setters for each span.

--- public/src/view/card-view.mjs
+++ public/src/view/card-view.mjs
@@ -8,12 +8,22 @@
 'use strict';
 
 export default class CardView {
+  /** @private @constant */
+  #spanElements = {};
 
   /**
    * 
    * @param {object} parent DOM node
    */
   constructor(parent) {
+    ['name', 'background', 'type', 'era'].forEach((e) => {
+      this.#spanElements[e] = document.createElement('span');
+      parent.append(this.#spanElements[e]);
 
+      // create setters on runtime
+      this[`setTextOf${e[0].toUpperCase() + e.slice(1)}`] = (newValue) => {
+        this.#spanElements[e].textContent = newValue;
+      }
+    });
   }
 }
