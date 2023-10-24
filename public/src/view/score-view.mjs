b223278dbf979e37f09eed181c622d52536f69a5
alu0101056944 <alu0101056944@ull.edu.es>
2023-10-24 11:52:25 +0000

    Implemented ScoreView.

--- public/src/view/score-view.mjs
+++ public/src/view/score-view.mjs
@@ -8,11 +8,48 @@
 'use strict';
 
 export default class ScoreView {
+  /** @private */
+  #score = undefined;
+  #scoreNode = undefined;
 
   /**
    * @param {object} parent DOM node
    */
   constructor(parent) {
-    
+    this.#score = 0;
+    const containerObject = this;
+    const cssLinkerNode = document.createElement('link');
+    cssLinkerNode.setAttribute('rel', 'stylesheet');
+    cssLinkerNode.setAttribute('href', 'styles/components/score.css');
+    cssLinkerNode.addEventListener('load', () => {
+      console.log('ScoreView css style loaded.');
+    });
+    parent.append(cssLinkerNode);
+    this.#scoreNode = document.createElement('p');
+    p.textContent = '0';
+    parent.append(this.#scoreNode);
+    const likeButton = document.createElement('button');
+    likeButton.textContent = 'Like';
+    likeButton.addEventListener('click', function() {
+      containerObject.setScore(containerObject.getScore() + 1);
+    });
+    parent.append(likeButton);
+    const dislikeButton = document.createElement('button');
+    dislikeButton.textContent = 'Dislike';
+    dislikeButton.addEventListener('click', function() {
+      if (container.getScore() > 0) {
+        containerObject.setScore(containerObject.getScore() - 1);
+      }
+    });
+    parent.append(dislikeButton);
+  }
+
+  setScore(newScore) {
+    this.#score = newScore;
+    this.#scoreNode.textContent = this.#score;
+  }
+
+  getScore() {
+    return this.#score;
   }
 }
