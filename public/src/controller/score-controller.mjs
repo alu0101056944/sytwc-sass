5b397f61eca0f2683f26061bdf07b779461231d2
alu0101056944 <alu0101056944@ull.edu.es>
2023-10-24 11:31:52 +0000

    Added ScoreWebcomp, ScoreController, ScoreModel, ScoreView classes.

---
+++ public/src/controller/score-controller.mjs
@@ -0,0 +1,30 @@
+/**
+ * @author Marcos Barrios
+ * @since 24_10_2023
+ * @desc Persistency of score.
+ * @module ScoreController
+ */
+
+'use strict';
+
+import ScoreModel from "../model/score-model.mjs";
+import ScoreView from "../view/score-view.mjs";
+
+/**
+ * Persistency of score.
+ */
+export default class ScoreController {
+
+  /** @private @constant */
+  #model = undefined;
+  #view = undefined;
+
+  /**
+   * @param {object} parent where the view will get appended to
+   */
+  constructor(parent) {
+    this.#model = new ScoreModel();
+    this.#view = new ScoreView(parent);
+  }
+
+}
---
+++ public/src/view/score-view.mjs
@@ -0,0 +1,18 @@
+/**
+ * @author Marcos Barrios
+ * @since 24_10_2023
+ * @brief Appearance of a score component in HTML/CSS
+ * @module ScoreView
+ */
+
+'use strict';
+
+export default class ScoreView {
+
+  /**
+   * @param {object} parent DOM node
+   */
+  constructor(parent) {
+    
+  }
+}
